dae1c3816d50eeb35d5dc4080d8fa8ccc4ef686e
Rich Thorp <rich@whitespace.studio>
2025-04-05 17:04:28 +0200

    Add basic index.php content

---
+++ dist/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: LAUDO Assignment
+Plugin URI: https://lau.do/
+Description: LAUDO
+Author: Rich Thorp
+Version: 1.0.0
+Requires Plugins: advanced-custom-fields-pro, gravityforms
+Author URI: https://richthorp-webdev.co.uk
+*/
+
+
+add_action('init', function () {
+  
+});
