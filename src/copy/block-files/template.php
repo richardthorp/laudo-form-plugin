658e25bd672b6901e7d17e107f9f56eac4ff703a
Rich Thorp <rich@whitespace.studio>
2025-04-05 17:48:47 +0200

    Add markup to template

--- src/copy/block-files/template.php
+++ src/copy/block-files/template.php
@@ -1,14 +1,14 @@
 <?php
+
 /**
  * LAUDO form block markup
  * 
  */
 
-$form = get_field('form_selection');
+$form_id = get_field('form_selection');
 
-// No form selected:
-if (!$form) {
-  // In the admin, show an error message
+// If there isn't a form selected, show an error message in the admin
+if (!$form_id) {
   if (is_admin()) { ?>
     <section style="text-align: center; background-color: #000; padding: 3rem 2rem;">
       <h2 style="color:#FFF">LAUDO form block</h2>
@@ -16,6 +16,35 @@ if (!$form) {
     </section>
 <?php
   }
-  // Exit the file
+  // On the front end and in the admin, exit the file
   return;
 }
+
+$sidebar_heading = get_field('sidebar_heading');
+$sidebar_heading_level = get_field('sidebar_heading_level');
+$sidebar_description = get_field('sidebar_description');
+$form_heading = get_field('form_heading');
+
+?>
+
+<div class="container">
+  <div class="sidebar">
+    <?php
+    if ($sidebar_heading) {
+      echo "<{$sidebar_heading_level} class='sidebar-heading'>";
+      echo $sidebar_heading;
+      echo "</{$sidebar_heading_level}>";
+    }
+
+    echo $sidebar_description ? '<p class="sidebar-description">' . $sidebar_description . '</p>' : '';
+    ?>
+  </div>
+  <div class="form-content">
+    <?= $form_heading ? '<p class="form-heading">' . $form_heading . '</p>' : ''; ?>
+    <?php
+    // Output the selected form
+    // See https://docs.gravityforms.com/adding-a-form-to-the-theme-file/#h-basic-function-call
+    gravity_form($form_id, false, false, false, '', true);
+    ?>
+  </div>
+</div>
