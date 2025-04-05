7a2189b5aea6a3bb2e2acf35f56ec655e7023d86
Rich Thorp <rich@whitespace.studio>
2025-04-05 17:37:31 +0200

    Handle no form selection in template

--- src/copy/block-files/template.php
+++ src/copy/block-files/template.php
@@ -1,2 +1,21 @@
 <?php
-echo 'TEMPLATE LOADED';
+/**
+ * LAUDO form block markup
+ * 
+ */
+
+$form = get_field('form_selection');
+
+// No form selected:
+if (!$form) {
+  // In the admin, show an error message
+  if (is_admin()) { ?>
+    <section style="text-align: center; background-color: #000; padding: 3rem 2rem;">
+      <h2 style="color:#FFF">LAUDO form block</h2>
+      <p style="color:#FFF">No form selected</p>
+    </section>
+<?php
+  }
+  // Exit the file
+  return;
+}
