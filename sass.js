40bd8e66ea8159413a8ce6da8b413001a397a9ae
Rich Thorp <rich@whitespace.studio>
2025-04-07 07:56:07 +0200

    Add editor styles

--- sass.js
+++ sass.js
@@ -6,7 +6,7 @@ var pkg = require('./package.json');
 // Configs
 var configs = {
 	name: 'BuildToolsCookbook',
-	files: ['main.scss'],
+	files: ['main.scss', 'editor.scss'],
 	pathIn: 'src/scss',
 	pathOut: 'dist/css',
 	indentType: 'tab',
